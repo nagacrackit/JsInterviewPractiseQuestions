8195428ffc20a88f45d28f6c1e5a2dad4f9d7aae
nagacrackit <nagarjunacrackit@gmail.com>
2018-12-23 21:44:04 +0530

    minMax problem

--- interviewQuestions.js
+++ interviewQuestions.js
@@ -64,3 +64,22 @@ stairCase=(n)=>{
 }
 
 stairCase(4);
+
+
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+//
+//     For example,arr=[1,3,5,7,9] . Our minimum sum is 1+3+5+7=16  and our maximum sum is 3+5+7+9=24. We would print
+//
+// 16 24
+
+minMax=(arr)=>{
+    arr.sort((a, b) => a - b);
+    let minSum = 0, maxSum = 0;
+    for (let i = 0; i < arr.length - 1; i++){
+        minSum += arr[i];
+        maxSum += arr[i + 1];
+    }
+    console.log(`${minSum} ${maxSum}`);
+}
+
+minMax([1,2,3,4,5])
